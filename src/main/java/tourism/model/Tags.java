9a50f20a1e7a8a02e8b2f9d1827b162e15975ab5
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-02-23 14:00:55 +0100

    tags enum

---
+++ src/main/java/tourism/model/Tags.java
@@ -0,0 +1,65 @@
+package tourism.model;
+
+public enum Tags {
+    FORLYSTELSESPARK{
+        @Override
+        public String toString() {
+            return "Forlystelsespark";
+        }
+    },
+    PARK{
+        @Override
+        public String toString() {
+            return "Park";
+        }
+    },
+    SPISESTEDER{
+        @Override
+        public String toString() {
+            return "Spisesteder";
+        }
+    },
+    UDENDØRS{
+        @Override
+        public String toString() {
+            return "Udendørs";
+        }
+    },
+    HC_ANDERSEN{
+        @Override
+        public String toString() {
+            return "H.C Andersen";
+        }
+    },
+    EVENTYR{
+        @Override
+        public String toString() {
+            return "Eventyr";
+        }
+    },
+    BØRN{
+        @Override
+        public String toString() {
+            return "Børn";
+        }
+    },
+    KUNST{
+        @Override
+        public String toString() {
+            return "Kunst";
+        }
+    },
+    MUSEUM{
+        @Override
+        public String toString() {
+            return "Museum";
+        }
+    },
+    INDENDØRS{
+        @Override
+        public String toString() {
+            return "Indendørs";
+        }
+    },
+
+}
