a3504dd9f7395d8e0a73692c513d9b25306a73e8
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-02-24 16:22:42 +0100

    Tags displayvalue added

--- src/main/java/tourism/model/Tags.java
+++ src/main/java/tourism/model/Tags.java
@@ -1,65 +1,25 @@
 package tourism.model;
 
 public enum Tags {
-    FORLYSTELSESPARK{
-        @Override
-        public String toString() {
-            return "Forlystelsespark";
-        }
-    },
-    PARK{
-        @Override
-        public String toString() {
-            return "Park";
-        }
-    },
-    SPISESTEDER{
-        @Override
-        public String toString() {
-            return "Spisesteder";
-        }
-    },
-    UDENDØRS{
-        @Override
-        public String toString() {
-            return "Udendørs";
-        }
-    },
-    HC_ANDERSEN{
-        @Override
-        public String toString() {
-            return "H.C Andersen";
-        }
-    },
-    EVENTYR{
-        @Override
-        public String toString() {
-            return "Eventyr";
-        }
-    },
-    BØRN{
-        @Override
-        public String toString() {
-            return "Børn";
-        }
-    },
-    KUNST{
-        @Override
-        public String toString() {
-            return "Kunst";
-        }
-    },
-    MUSEUM{
-        @Override
-        public String toString() {
-            return "Museum";
-        }
-    },
-    INDENDØRS{
-        @Override
-        public String toString() {
-            return "Indendørs";
-        }
-    },
+    FORLYSTELSESPARK("Forlystelsespark"),
+    PARK("Park"),
+    SPISESTEDER("Spisesteder"),
+    UDENDØRS("Udendørs"),
+    HC_ANDERSEN("H.C Andersen"),
+    EVENTYR("Eventyr"),
+    BØRN("Børn"),
+    KUNST("Kunst"),
+    MUSEUM("Museum"),
+    INDENDØRS("Indendørs");
+
+    private final String displayValue;
+
+    private Tags(String displayValue) {
+        this.displayValue = displayValue;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
 
 }
